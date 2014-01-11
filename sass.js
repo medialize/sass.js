19d131d9e922efc4d7a8af64308ceaba9b5c2867
asciidisco <public@asciidisco.com>
2014-01-11 18:19:17 +0100

    Fixed issues with PR #1

--- sass.js
+++ sass.js
@@ -80,75 +80,6 @@ this.Sass = (function(){
       }
       
       return result;
-    },
-
-    compileOnLoad: function () {
-      //
-      // Get all <link> tags with the 'rel' attribute set to "stylesheet/sass"
-      //
-      var links = document.getElementsByTagName('link'),
-        head = document.getElementsByTagName('head')[0],
-        linksLength = links.length,
-        that = this,
-        sheetsLength = 0,
-        sheets = [], i, j;
-
-      var ecb = function (status, url) {
-        console.error('Error compiling sheet', 'Status:', status, 'Url:', url);
-      };
-
-      var cb = function (data) {
-        var cpcss = that.compile(data);
-        var css = document.createElement('style');
-        css.setAttribute('type', 'text/css');
-        css.appendChild(document.createTextNode(cpcss));
-        head.appendChild(css);
-      };
-
-      if (linksLength) {
-        for (i = 0; i < linksLength; i++) {
-          if (links[i].rel === 'stylesheet/sass') {
-            sheets.push(links[i]);
-          }
-        }
-
-        sheetsLength = sheets.length;
-
-        for (j = 0; j < sheetsLength; j++) {
-          this._doXHR(sheets[j].href, cb, ecb);
-        }
-      }
-    },
-
-    _getXMLHttpRequest: function () {
-      if (window.XMLHttpRequest) {
-        return new XMLHttpRequest();
-      } else {
-        try {
-          /*global ActiveXObject */
-          return new ActiveXObject("MSXML2.XMLHTTP.3.0");
-        } catch (e) {
-          console.error('browser does not support AJAX.');
-            return null;
-        }
-      }
-    },
-
-    _doXHR: function (url, callback, errback) {
-      var xhr = this._getXMLHttpRequest();
-      xhr.open('GET', url, false);
-      xhr.setRequestHeader('Accept', 'text/x-sass, text/css; q=0.9, */*; q=0.5');
-      xhr.send(null);
-
-      function handleResponse(xhr, callback, errback) {
-        if (xhr.status >= 200 && xhr.status < 300) {
-          callback(xhr.responseText);
-        } else {
-          errback(xhr.status, url);
-        }
-      }
-
-      handleResponse(xhr, callback, errback);
     }
 
   };
@@ -157,8 +88,3 @@ this.Sass = (function(){
   return Sass;
   
 })();
-
-// compile sheets on the fly
-if (window && window.Sass) {
-  window.Sass.compileOnLoad();
-}
