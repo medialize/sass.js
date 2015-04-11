361f0701f4ae98789db55d92c5b5f0f5773d75c7
Rodney Rehm <mail+github@rodneyrehm.de>
2015-04-11 15:56:42 +0200

    feature(build): identify and save versions used to build sass.js

---
+++ grunt-tasks/versions.js
@@ -0,0 +1,107 @@
+module.exports = function GruntfileVersions(grunt) {
+  'use strict';
+
+  var childProcess = require('child_process');
+  var fs = require('fs');
+
+  function getGitMeta(path, done) {
+    // https://github.com/damkraw/grunt-gitinfo
+    var data = {
+      version: null,
+      commit: null,
+    };
+
+    // git rev-parse --short HEAD
+    // f82a41b
+    childProcess.exec('git rev-parse --short HEAD', {cwd: path}, function (err, stdout, stderr){
+      if (err) {
+        grunt.log.error("obtaining git commit failed with: " + err.code + '\n' + strderr);
+        return;
+      }
+
+      data.commit = stdout.slice(0, -1);
+      // git symbolic-ref HEAD
+      // refs/heads/master
+      childProcess.exec('git symbolic-ref HEAD', {cwd: path}, function (err, stdout, stderr){
+        if (err) {
+          // git describe --tags
+          // 3.2.0-beta.5-32-g0dd6543 (last tag contained)
+          // 3.2.0-beta.5 (immediate tag)
+          childProcess.exec('git describe --tags', {cwd: path}, function (err, stdout, stderr){
+            if (err) {
+              grunt.log.error("obtaining git tag failed with: " + err.code + '\n' + strderr);
+              return;
+            }
+
+            data.version = stdout.slice(0, -1);
+            done(data);
+          });
+
+          return;
+        }
+
+        data.version = stdout.replace('refs/heads/', '').slice(0, -1);
+        done(data);
+      });
+    });
+  }
+
+  grunt.registerTask('get-emscripten-version', function () {
+    var done = this.async();
+
+    childProcess.exec('emcc --version', function (err, stdout, stderr){
+      if (err) {
+        grunt.log.error("`emcc --version` failed with: " + err.code + '\n' + strderr);
+        return;
+      }
+
+      var line = stdout.split('\n')[0];
+      // "emcc (Emscripten GCC-like replacement) 1.30.2 (commit dac9f88335dd74b377bedbc2ad7d3b64f0c9bb15)"
+      var tokens = line.match(/emcc \([^)]+\) ([\d.]+) \(commit ([^)]+)\)/);
+
+      grunt.config.data.emscripten_version = {
+        version: tokens[1],
+        commit: tokens[2].slice(0, 7),
+      };
+
+      done();
+    });
+  });
+
+  grunt.registerTask('get-libsass-version', function () {
+    var done = this.async();
+
+    getGitMeta('libsass/libsass/', function(data) {
+      grunt.config.data.libsass_version = data;
+      done();
+    });
+  });
+
+  grunt.registerTask('get-sass-version', function () {
+    var done = this.async();
+
+    getGitMeta('.', function(data) {
+      grunt.config.data.sassjs_version = data;
+      done();
+    });
+  });
+
+  grunt.registerTask('save-versions', function () {
+    var done = this.async();
+    var versions = {
+      emscripten: grunt.config.data.emscripten_version,
+      libsass: grunt.config.data.libsass_version,
+      sassjs: grunt.config.data.sassjs_version,
+    };
+    var _versions = JSON.stringify(versions, null, 2);
+    fs.writeFile('dist/versions.json', _versions, done);
+  });
+
+  grunt.registerTask('versions', [
+    'get-emscripten-version',
+    'get-libsass-version',
+    'get-sass-version',
+    'save-versions'
+  ]);
+
+};
