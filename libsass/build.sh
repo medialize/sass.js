99c2b17faa953eacb683a2530eec3c1ec708bc93
Rodney Rehm <mail+github@rodneyrehm.de>
2015-05-17 15:51:14 +0200

    feature(emscripten): adding sass values webidl to build

--- libsass/build.sh
+++ libsass/build.sh
@@ -12,7 +12,7 @@ echo "Building libsass version ${LIBSASS_VERSION} ${2:-}"
 
 # clean
 echo "  resetting target directory to git HEAD"
-(cd libsass && git checkout --force ${LIBSASS_VERSION})
+(cd ./libsass && git checkout --force ${LIBSASS_VERSION})
 
 # patch
 echo "  patching Makefile"
@@ -20,6 +20,27 @@ patch ./libsass/Makefile < ./Makefile.patch
 echo "  copying emscripten_wrapper"
 cp ./emscripten_wrapper.cpp ./libsass/emscripten_wrapper.cpp
 cp ./emscripten_wrapper.hpp ./libsass/emscripten_wrapper.hpp
+cp ./emscripten_sass_values.cpp ./libsass/emscripten_sass_values.cpp
+cp ./emscripten_sass_values.hpp ./libsass/emscripten_sass_values.hpp
+cp ./emscripten_sass_values.idl ./libsass/emscripten_sass_values.idl
+cp ./emscripten_sass_values_glue_wrapper.cpp ./libsass/emscripten_sass_values_glue_wrapper.cpp
+
+# build WebIDL
+# (cd ./libsass && python /usr/local/Cellar/emscripten/HEAD/libexec/tools/webidl_binder.py \
+#   emscripten_sass_values.idl \
+#   emscripten_sass_values_glue)
+# # yes, I imagine someone is going to kill me for this heresy
+# echo "#include \"emscripten_sass_values.hpp\" \n #include <cstdlib> \n #include <cstring>\n $(cat libsass/emscripten_sass_values_glue.cpp)" > libsass/emscripten_sass_values_glue.cpp
+
+# need to add imports:
+#include "emscripten_sass_values.hpp"
+#include <cstdlib>
+#include <cstring>
+#include <emscripten.h>
+# need to fix all functions returning (char* EMSCRIPTEN_KEEPALIVE):
+#     return self->value;
+# to
+#     return strdup(self->value.c_str());
 
 # build
 echo "  initializing emscripten"
---
+++ libsass/emscripten_sass_values_glue_wrapper.cpp
@@ -0,0 +1,4 @@
+// #include "sass_values.h"
+// #include <emscripten_sass_values.hpp>
+// #include <emscripten_sass_values_glue.cpp>
+// // explained in http://kripken.github.io/emscripten-site/docs/porting/connecting_cpp_and_javascript/WebIDL-Binder.html#compiling-the-project-using-the-bindings-glue-code
