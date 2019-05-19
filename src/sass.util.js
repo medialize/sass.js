e0dcb6de8e4a26197264d8e56b991aa577f1680e
Rodney Rehm <mail+github@rodneyrehm.de>
2019-05-19 12:03:57 +0200

    chore(emscripten): upgrading to emscripten 1.38.31

--- src/sass.util.js
+++ src/sass.util.js
@@ -23,7 +23,7 @@ function addTrailingSlash(text) {
 
 function pointerToString(pointer) {
   /*jshint camelcase:false*/
-  return pointer && Module.Pointer_stringify(pointer) || null;
+  return pointer && Module.UTF8ToString(pointer) || null;
 }
 
 function stringToPointer(text) {
